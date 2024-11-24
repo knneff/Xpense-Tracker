663896db94ab1a77b4e9140f58269e83a050e35f
knmnltzy <knmnltzy@gmail.com>
2024-11-24 08:00:02 +0800

    nasa baba custom_style.css ung bgGreen2

--- views/partials/headNoSide.php
+++ views/partials/headNoSide.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expense Tracker</title>
-    <script src="/styles/scripts.js"></script>
-    <link rel="stylesheet" href="/styles/style.css">
+    <script src="styles/scripts.js"></script>
+    <link rel="stylesheet" href="styles/custom_style.css">
 </head>
 
 <body class="bgGreen h-full">
