0339163d83ecfa8f319b3036005c35c6e64563da
knmnltzy <knmnltzy@gmail.com>
2024-11-27 22:27:57 +0800

    add transition, lagyan nalang bgGreen ung body since ung ibang content is not h-full

--- views/partials/bodyNoBars.php
+++ views/partials/bodyNoBars.php
@@ -1,6 +1,6 @@
 <?php require('head.php') ?>
 
-<body class=" h-full overflow-auto">
+<body class=" h-full overflow-auto bgGreen">
     <!-- NAV BAR IS HERE -->
     <div class="h-full">
         <!-- DITO NA YUNG PINAKACONTENT NINYO -->
--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -35,7 +35,7 @@
     <hr class="my-2 mx-2 border-gray-600">
 
     <!-- HOME ICON -->
-    <a href="/dashboard" title="Dashboard" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/dashboard") ? 'bg-emerald-900' : '' ?>">
+    <a href="/dashboard" title="Dashboard" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/dashboard") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
             <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
@@ -44,7 +44,7 @@
     </a>
 
     <!-- EXPENSE LOG LOG ICON-->
-    <a href="/expenselog" title="Expense Log" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/expenselog") ? 'bg-emerald-900' : '' ?>">
+    <a href="/expenselog" title="Expense Log" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/expenselog") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z" clip-rule="evenodd" />
             <path d="M18.75 6.75h1.875c.621 0 1.125.504 1.125 1.125V18a1.5 1.5 0 0 1-3 0V6.75Z" />
@@ -53,7 +53,7 @@
     </a>
 
     <!-- STATISTICS -->
-    <a href="/statistics" title="Statistics" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/statistics") ? 'bg-emerald-900' : '' ?>">
+    <a href="/statistics" title="Statistics" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/statistics") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z" clip-rule="evenodd" />
@@ -62,7 +62,7 @@
     </a>
 
     <!-- GOAL -->
-    <a href="/goal" title="Goals and Plans" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/goal") ? 'bg-emerald-900' : '' ?>">
+    <a href="/goal" title="Goals and Plans" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/goal") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clip-rule="evenodd" />
         </svg>
@@ -70,7 +70,7 @@
     </a>
 
     <!-- USER GROUPS ICON -->
-    <a href="/shared" title="Shared Expense" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/shared") ? 'bg-emerald-900' : '' ?>">
+    <a href="/shared" title="Shared Expense" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/shared") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" />
             <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
@@ -79,7 +79,7 @@
     </a>
 
     <!-- AUDIT LOG -->
-    <a href="/audit" title="Audit Log" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/audit") ? 'bg-emerald-900' : '' ?>">
+    <a href="/audit" title="Audit Log" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/audit") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
         </svg>
@@ -87,7 +87,7 @@
     </a>
 
     <!-- SYSTEM SETTINGS -->
-    <a href="/settings" title="System Settings" class="hover:bg-emerald-900 flex flex-row items-center <?= isUri("/settings") ? 'bg-emerald-900' : '' ?>">
+    <a href="/settings" title="System Settings" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/settings") ? 'bg-emerald-900' : '' ?>">
         <svg class="textGray w-12 p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clip-rule="evenodd" />
         </svg>
