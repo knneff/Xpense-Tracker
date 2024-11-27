ba89079542dfc5389eedf9ac62a0ef4a62f9662a
kit <keithrussel632@gmail.com>
2024-11-27 23:36:47 +0800

    hide x overvlow

--- views/partials/bodyNoBars.php
+++ views/partials/bodyNoBars.php
@@ -1,6 +1,6 @@
 <?php require('head.php') ?>
 
-<body class=" h-full overflow-auto bgGreen">
+<body class=" h-full overflow-auto bgGreen overflow-x-hidden">
     <!-- NAV BAR IS HERE -->
     <div class="h-full">
         <!-- DITO NA YUNG PINAKACONTENT NINYO -->
