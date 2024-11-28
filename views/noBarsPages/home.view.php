280844e3c2a3e422d6403c3405ec51043ddc69d3
kit <keithrussel632@gmail.com>
2024-11-28 16:18:47 +0800

    static landing page done

--- views/noBarsPages/home.view.php
+++ views/noBarsPages/home.view.php
@@ -48,52 +48,52 @@
         </div>
       </section>
     <!-- SVG Separator -->
-    <section class="bg-[#1a664d] h-[640px] w-screen flex items-center p-8">
+    <section class="bg-[#064e3b] h-[640px] w-screen flex items-center p-8">
         <div class="absolute left-52 w-1/2">
           <div>
-            <img src="assets\main-grocery-store-1-300x208.png" alt="Grocery store image" class="h-24 md:h-48 lg:h-52">
+            <img src="assets\main-grocery-store-1-300x208.png" alt="Grocery store image" class="w-[300px] h-[230px]">
           </div>
         </div>
         
-        <div class="w-1/2 absolute left-[700px]">
-          <div class="w-2/3">
-            <h1 class="text-xl md:text-2xl lg:text-4xl font-bold mb-5">How it works</h1>
+        <div class="w-1/2 absolute left-[670px]">
+          <div class="w-3/4">
+            <h1 class="text-4xl font-bold mb-5">How it works</h1>
             <hr>
-            <p class="text-base md:text-lg lg:text-xl text-justify">
+            <br>
+            <p class="text-2xl leading-6">
               Xpense Tracker is a powerful yet easy-to-use web application designed to help you track, manage, and optimize your finances.
             </p>
             <br>
-            <p class="text-base md:text-lg lg:text-xl text-justify">
+            <br>
+            <p class="text-2xl leading-6">
               Using intuitive tools for budgeting, expense categorization, and real-time insights, Xpense Tracker empowers you to take control of your money and achieve your financial goals effortlessly.
             </p>
           </div>
         </div>
     </section>
-    <section>
-      <div class="text-center text-2xl font-bold bg-[#1a664d] p-5 w-96 h-20 border-4 rounded-full">
-          <h1>PRODUCT FEATURES</h1>
+    <section class="px-36">
+      <div class="p-10">
+        <h1 class="text-center text-5xl font-bold">Features</h1>
       </div>
-    <br><br>
-    </section>
-    <section>
-      <div class="bg-[#d5ffdf] h-fit flex">
+      <hr>
+      <div class="bgGreen h-fit flex">
         <div class="m-10">
           <div>
-            <h1 class="text-[#1a664d] text-xl md:text-2xl lg:text-4xl font-bold">Track Your Way to Financial Freedom</h1>
+            <h1 class="text-gray-300 text-xl md:text-2xl lg:text-4xl font-bold">Track Your Way to Financial Freedom</h1>
           </div>
-          <div class="text-justify text-[#1a664d]">
+          <div class="text-justify text-gray-300">
             <h1 class="text-xs md:text-sm lg:text-base">With Financial Goals, you can set, track, and achieve your financial milestones with ease. Whether you're saving for a vacation, building an emergency fund, or working towards a major investment, this feature helps you stay focused, visualize progress, and adjust your strategy to ensure success. Stay on top of your goals and turn your financial dreams into reality. </h1>
           </div>
         </div>
         <div class="pr-10 md:pr-52 py-10">
-        <svg class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" xmlns="http://www.w3.org/2000/svg" viewBox="-4.8 -4.8 57.60 57.60" id="a" fill="#000000">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" viewBox="-4.8 -4.8 57.60 57.60" id="a" fill="#000000">
           <g id="SVGRepo_bgCarrier" stroke-width="0">
-            <rect x="-4.8" y="-4.8" width="57.60" height="57.60" rx="28.8" fill="#70db99" strokewidth="0"/>
+            <rect x="-4.8" y="-4.8" width="57.60" height="57.60" rx="28.8" fill="#1e5b50" strokewidth="0"/>
           </g>
           <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
           <g id="SVGRepo_iconCarrier">
             <defs>
-              <style>.f{fill:none;stroke:#000000;stroke-linecap:round;stroke-linejoin:round;}</style>
+            <style>.f{fill:none;stroke:#71dc9a;stroke-linecap:round;stroke-linejoin:round;}</style>
             </defs>
             <rect id="b" class="f" x="6.9608" y="13.065" width="7.5178" height="21.87"/>
             <rect id="c" class="f" x="20.7926" y="18.5325" width="7.5178" height="16.4025"/>
@@ -103,11 +103,11 @@
         </svg>
         </div>
       </div>
-      <div class="bg-[#1a664d] h-fit flex">
+      <div class="bg-[#072822] h-fit flex rounded-xl">
         <div class="pl-10 md:pl-52 py-10">
           <svg class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-127.03 -127.03 762.16 762.16" xml:space="preserve" fill="#000000">
             <g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(0,0), scale(1)">
-              <rect x="-127.03" y="-127.03" width="762.16" height="762.16" rx="381.08" fill="#70db99" strokewidth="0"></rect>
+              <rect x="-127.03" y="-127.03" width="762.16" height="762.16" rx="381.08" fill="#1e5b50" strokewidth="0"></rect>
             </g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="1.0162"></g>
             <g id="SVGRepo_iconCarrier">
@@ -142,19 +142,19 @@
           </div>
         </div>
       </div>
-      <div class="bg-[#d5ffdf] h-fit flex">
+      <div class="bgGreen h-fit flex">
         <div class="m-10">
           <div>
-            <h1 class="text-[#1a664d] text-xl md:text-2xl lg:text-4xl font-bold">Never Miss a Payment, Stay in Control of Subscriptions</h1>
+            <h1 class="text-gray-300 text-xl md:text-2xl lg:text-4xl font-bold">Never Miss a Payment, Stay in Control of Subscriptions</h1>
           </div>
-          <div class="text-justify text-[#1a664d]">
+          <div class="text-justify text-gray-300">
             <h1 class="text-xs md:text-sm lg:text-base">With the Subscription List, you can keep a detailed record of all your recurring subscriptions, whether it's streaming services, software, or memberships. Get reminders before payments are due and track your spending to see which subscriptions you use most. This feature helps you cut out unnecessary expenses and stay on top of renewals, ensuring you only pay for what you really need.</h1>
           </div>
         </div>
         <div class="pr-10 md:pr-52 py-10">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" viewBox="-102.4 -102.4 1228.80 1228.80" class="icon" version="1.1" fill="#000000">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-102.4 -102.4 1228.80 1228.80" class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" version="1.1" fill="#000000">
           <g id="SVGRepo_bgCarrier" stroke-width="0">
-            <rect x="-102.4" y="-102.4" width="1228.80" height="1228.80" rx="614.4" fill="#70db99" strokewidth="0"/>
+          <rect x="-102.4" y="-102.4" width="1228.80" height="1228.80" rx="614.4" fill="#1e5b50" strokewidth="0"/>
           </g>
           <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
           <g id="SVGRepo_iconCarrier">
@@ -180,25 +180,25 @@
         </svg>
         </div>
       </div>
-      <div class="bg-[#1a664d] h-fit flex">
+      <div class="bg-[#072822] h-fit flex rounded-xl">
         <div class="pl-10 md:pl-52 py-10">
           <svg class="w-[8rem] h-[8rem] md:w-[9rem] md:h-[9rem] lg:w-[11rem] lg:h-[11rem]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-51.2 -51.2 614.40 614.40" xml:space="preserve" fill="#000000">
             <g id="SVGRepo_bgCarrier" stroke-width="0">
-              <rect x="-51.2" y="-51.2" width="614.40" height="614.40" rx="307.2" fill="#70db99" strokewidth="0"></rect>
+              <rect x="-51.2" y="-51.2" width="614.40" height="614.40" rx="307.2" fill="#1e5b50" strokewidth="0"></rect>
             </g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
               <g>
-                <path style="fill:#00B8B8;" d="M256.001,98.244L256.001,98.244c10.954,0,21.583,1.388,31.721,3.996V69.405 c0-17.519-14.202-31.721-31.721-31.721l0,0c-17.519,0-31.721,14.202-31.721,31.721v32.835 C234.418,99.632,245.047,98.244,256.001,98.244z"></path>
-                <polygon style="fill:#00B8B8;" points="417.207,405.793 94.793,405.793 129.088,315.654 382.914,315.654 "></polygon>
-                <circle style="fill:#00B8B8;" cx="255.997" cy="440.073" r="34.251"></circle>
+                <path style="fill:#D1D5DB;" d="M256.001,98.244L256.001,98.244c10.954,0,21.583,1.388,31.721,3.996V69.405 c0-17.519-14.202-31.721-31.721-31.721l0,0c-17.519,0-31.721,14.202-31.721,31.721v32.835 C234.418,99.632,245.047,98.244,256.001,98.244z"></path>
+                <polygon style="fill:#D1D5DB;" points="417.207,405.793 94.793,405.793 129.088,315.654 382.914,315.654 "></polygon>
+                <circle style="fill:#D1D5DB;" cx="255.997" cy="440.073" r="34.251"></circle>
               </g>
               <g>
-                <path style="fill:#00B8B8;" d="M429.577,400.155l-33.984-89.325c-2.004-5.263-7.047-8.741-12.679-8.741H142.629v-76.91 c0-54.637,38.851-100.367,90.377-111.022c0.111-0.023,0.221-0.046,0.332-0.068c1.715-0.35,3.444-0.658,5.187-0.929 c0.296-0.045,0.591-0.081,0.887-0.125c1.308-0.193,2.625-0.355,3.946-0.503c0.555-0.061,1.108-0.129,1.663-0.182 c1.399-0.134,2.805-0.239,4.219-0.321c0.75-0.043,1.502-0.076,2.253-0.104c0.952-0.038,1.909-0.058,2.868-0.073 c1.092-0.015,2.183-0.015,3.275,0c0.959,0.014,1.915,0.035,2.868,0.073c0.751,0.03,1.503,0.061,2.253,0.104 c1.413,0.084,2.82,0.187,4.219,0.321c0.555,0.053,1.108,0.121,1.663,0.182c1.321,0.148,2.638,0.311,3.946,0.503 c0.296,0.043,0.591,0.08,0.887,0.125c1.742,0.271,3.471,0.579,5.187,0.929c0.111,0.022,0.221,0.046,0.332,0.068 c51.526,10.657,90.377,56.385,90.377,111.022v37.166c0,7.492,6.074,13.565,13.565,13.565s13.565-6.073,13.565-13.565v-37.166 c0-61.633-39.893-114.123-95.213-133.009V69.405c0-24.97-20.316-45.286-45.286-45.286s-45.286,20.316-45.286,45.286V92.17 c-55.32,18.886-95.213,71.376-95.213,133.009v88.036l-33.386,87.754c-1.586,4.168-1.021,8.851,1.511,12.523 c2.531,3.672,6.706,5.865,11.167,5.865h118.116c-3.025,6.27-4.722,13.296-4.722,20.711c0,26.365,21.45,47.814,47.814,47.814 s47.814-21.449,47.814-47.813c0-7.415-1.697-14.441-4.722-20.711h118.116c0.011,0,0.022,0,0.027,0 c7.492,0,13.565-6.073,13.565-13.565C430.799,403.781,430.362,401.873,429.577,400.155z M237.844,85.846V69.405 c0-10.011,8.144-18.157,18.157-18.157c10.012,0,18.157,8.144,18.157,18.157v16.441c-0.065-0.008-0.129-0.014-0.194-0.022 c-1.324-0.17-2.655-0.317-3.989-0.45c-0.41-0.041-0.822-0.073-1.232-0.11c-0.982-0.088-1.968-0.17-2.956-0.237 c-0.469-0.033-0.939-0.062-1.408-0.09c-0.981-0.058-1.964-0.104-2.95-0.142c-0.419-0.016-0.838-0.035-1.259-0.047 c-1.385-0.041-2.774-0.066-4.168-0.066c-1.394,0-2.784,0.026-4.168,0.066c-0.421,0.012-0.84,0.033-1.259,0.047 c-0.986,0.038-1.97,0.084-2.95,0.142c-0.469,0.027-0.939,0.057-1.408,0.09c-0.989,0.068-1.974,0.149-2.956,0.237 c-0.411,0.037-0.822,0.069-1.232,0.11c-1.335,0.133-2.665,0.281-3.989,0.45C237.973,85.832,237.908,85.838,237.844,85.846z M256.001,460.753c-11.405,0-20.684-9.278-20.684-20.684c0-11.405,9.278-20.684,20.684-20.684s20.684,9.278,20.684,20.684 C276.684,451.474,267.406,460.753,256.001,460.753z M114.467,392.228l23.972-63.01h235.123l23.972,63.01H114.467z"></path>
-                <path style="fill:#00B8B8;" d="M429.565,307.849c-4.136,0-8.219-1.884-10.882-5.453c-4.48-6.005-3.246-14.504,2.759-18.984 c8.747-6.527,13.763-16.518,13.763-27.411s-5.016-20.885-13.763-27.411c-6.005-4.48-7.24-12.979-2.759-18.984 c4.48-6.004,12.98-7.241,18.984-2.759c15.446,11.526,24.669,29.901,24.669,49.154s-9.223,37.629-24.669,49.154 C435.232,306.971,432.386,307.849,429.565,307.849z"></path>
-                <path style="fill:#00B8B8;" d="M466.412,339.362c-3.814,0-7.603-1.598-10.286-4.715c-4.889-5.678-4.249-14.243,1.43-19.131 c17.359-14.944,27.316-36.637,27.316-59.516s-9.957-44.57-27.316-59.516c-5.678-4.889-6.317-13.452-1.43-19.131 c4.889-5.677,13.451-6.317,19.131-1.43c23.352,20.105,36.745,49.291,36.745,80.076s-13.393,59.972-36.745,80.076 C472.696,338.282,469.545,339.362,466.412,339.362z"></path>
-                <path style="fill:#00B8B8;" d="M82.435,307.849c-2.821,0-5.667-0.876-8.102-2.694c-15.446-11.526-24.669-29.901-24.669-49.154 c0-19.253,9.223-37.629,24.669-49.154c6.004-4.48,14.502-3.245,18.984,2.759c4.48,6.005,3.246,14.504-2.759,18.984 c-8.747,6.527-13.763,16.518-13.763,27.411c0,10.893,5.016,20.885,13.763,27.411c6.005,4.48,7.24,12.979,2.759,18.984 C90.656,305.965,86.571,307.849,82.435,307.849z"></path>
-                <path style="fill:#00B8B8;" d="M45.588,339.362c-3.135,0-6.282-1.08-8.844-3.284C13.393,315.973,0,286.785,0,256.001 s13.393-59.972,36.745-80.076c5.678-4.889,14.244-4.246,19.131,1.43c4.889,5.678,4.249,14.243-1.43,19.131 C37.086,211.43,27.13,233.123,27.13,256.001c0,22.879,9.957,44.57,27.316,59.516c5.678,4.889,6.317,13.452,1.43,19.131 C53.192,337.762,49.401,339.362,45.588,339.362z"></path>
+                <path style="fill:#D1D5DB;" d="M429.577,400.155l-33.984-89.325c-2.004-5.263-7.047-8.741-12.679-8.741H142.629v-76.91 c0-54.637,38.851-100.367,90.377-111.022c0.111-0.023,0.221-0.046,0.332-0.068c1.715-0.35,3.444-0.658,5.187-0.929 c0.296-0.045,0.591-0.081,0.887-0.125c1.308-0.193,2.625-0.355,3.946-0.503c0.555-0.061,1.108-0.129,1.663-0.182 c1.399-0.134,2.805-0.239,4.219-0.321c0.75-0.043,1.502-0.076,2.253-0.104c0.952-0.038,1.909-0.058,2.868-0.073 c1.092-0.015,2.183-0.015,3.275,0c0.959,0.014,1.915,0.035,2.868,0.073c0.751,0.03,1.503,0.061,2.253,0.104 c1.413,0.084,2.82,0.187,4.219,0.321c0.555,0.053,1.108,0.121,1.663,0.182c1.321,0.148,2.638,0.311,3.946,0.503 c0.296,0.043,0.591,0.08,0.887,0.125c1.742,0.271,3.471,0.579,5.187,0.929c0.111,0.022,0.221,0.046,0.332,0.068 c51.526,10.657,90.377,56.385,90.377,111.022v37.166c0,7.492,6.074,13.565,13.565,13.565s13.565-6.073,13.565-13.565v-37.166 c0-61.633-39.893-114.123-95.213-133.009V69.405c0-24.97-20.316-45.286-45.286-45.286s-45.286,20.316-45.286,45.286V92.17 c-55.32,18.886-95.213,71.376-95.213,133.009v88.036l-33.386,87.754c-1.586,4.168-1.021,8.851,1.511,12.523 c2.531,3.672,6.706,5.865,11.167,5.865h118.116c-3.025,6.27-4.722,13.296-4.722,20.711c0,26.365,21.45,47.814,47.814,47.814 s47.814-21.449,47.814-47.813c0-7.415-1.697-14.441-4.722-20.711h118.116c0.011,0,0.022,0,0.027,0 c7.492,0,13.565-6.073,13.565-13.565C430.799,403.781,430.362,401.873,429.577,400.155z M237.844,85.846V69.405 c0-10.011,8.144-18.157,18.157-18.157c10.012,0,18.157,8.144,18.157,18.157v16.441c-0.065-0.008-0.129-0.014-0.194-0.022 c-1.324-0.17-2.655-0.317-3.989-0.45c-0.41-0.041-0.822-0.073-1.232-0.11c-0.982-0.088-1.968-0.17-2.956-0.237 c-0.469-0.033-0.939-0.062-1.408-0.09c-0.981-0.058-1.964-0.104-2.95-0.142c-0.419-0.016-0.838-0.035-1.259-0.047 c-1.385-0.041-2.774-0.066-4.168-0.066c-1.394,0-2.784,0.026-4.168,0.066c-0.421,0.012-0.84,0.033-1.259,0.047 c-0.986,0.038-1.97,0.084-2.95,0.142c-0.469,0.027-0.939,0.057-1.408,0.09c-0.989,0.068-1.974,0.149-2.956,0.237 c-0.411,0.037-0.822,0.069-1.232,0.11c-1.335,0.133-2.665,0.281-3.989,0.45C237.973,85.832,237.908,85.838,237.844,85.846z M256.001,460.753c-11.405,0-20.684-9.278-20.684-20.684c0-11.405,9.278-20.684,20.684-20.684s20.684,9.278,20.684,20.684 C276.684,451.474,267.406,460.753,256.001,460.753z M114.467,392.228l23.972-63.01h235.123l23.972,63.01H114.467z"></path>
+                <path style="fill:#D1D5DB;" d="M429.565,307.849c-4.136,0-8.219-1.884-10.882-5.453c-4.48-6.005-3.246-14.504,2.759-18.984 c8.747-6.527,13.763-16.518,13.763-27.411s-5.016-20.885-13.763-27.411c-6.005-4.48-7.24-12.979-2.759-18.984 c4.48-6.004,12.98-7.241,18.984-2.759c15.446,11.526,24.669,29.901,24.669,49.154s-9.223,37.629-24.669,49.154 C435.232,306.971,432.386,307.849,429.565,307.849z"></path>
+                <path style="fill:#D1D5DB;" d="M466.412,339.362c-3.814,0-7.603-1.598-10.286-4.715c-4.889-5.678-4.249-14.243,1.43-19.131 c17.359-14.944,27.316-36.637,27.316-59.516s-9.957-44.57-27.316-59.516c-5.678-4.889-6.317-13.452-1.43-19.131 c4.889-5.677,13.451-6.317,19.131-1.43c23.352,20.105,36.745,49.291,36.745,80.076s-13.393,59.972-36.745,80.076 C472.696,338.282,469.545,339.362,466.412,339.362z"></path>
+                <path style="fill:#D1D5DB;" d="M82.435,307.849c-2.821,0-5.667-0.876-8.102-2.694c-15.446-11.526-24.669-29.901-24.669-49.154 c0-19.253,9.223-37.629,24.669-49.154c6.004-4.48,14.502-3.245,18.984,2.759c4.48,6.005,3.246,14.504-2.759,18.984 c-8.747,6.527-13.763,16.518-13.763,27.411c0,10.893,5.016,20.885,13.763,27.411c6.005,4.48,7.24,12.979,2.759,18.984 C90.656,305.965,86.571,307.849,82.435,307.849z"></path>
+                <path style="fill:#D1D5DB;" d="M45.588,339.362c-3.135,0-6.282-1.08-8.844-3.284C13.393,315.973,0,286.785,0,256.001 s13.393-59.972,36.745-80.076c5.678-4.889,14.244-4.246,19.131,1.43c4.889,5.678,4.249,14.243-1.43,19.131 C37.086,211.43,27.13,233.123,27.13,256.001c0,22.879,9.957,44.57,27.316,59.516c5.678,4.889,6.317,13.452,1.43,19.131 C53.192,337.762,49.401,339.362,45.588,339.362z"></path>
               </g>
             </g>
           </svg>
