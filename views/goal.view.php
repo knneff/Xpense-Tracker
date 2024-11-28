02321a450e5a8a3c488e6d913d6d0cd5704a6673
knmnltzy <knmnltzy@gmail.com>
2024-11-28 15:25:03 +0800

    goals btn

--- views/goal.view.php
+++ views/goal.view.php
@@ -4,16 +4,6 @@
     <p class="text-gray-300 my-4 text-center text-xl "><?= $message ?></p>
 <?php endif; ?>
 <main class="grid grid-cols-1 lg:grid-cols-2 gap-7">
-    <button
-        class="active:scale-95 transition-transform hover:scale-[1.03]"
-        type="button"
-        onclick="showPanelGoal()">
-        <div class="flex items-center justify-center border-2 border-dashed hover:border-white border-gray-300 rounded-lg h-20 sm:h-24">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="text-gray-300 w-10 h-10 sm:w-14 sm:h-14">
-                <path d="M4 12H20M12 4V20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
-        </div>
-    </button>
     <?php foreach ($goals as $goal) : ?>
         <?php $percent = floor($goal['paidAmount'] / $goal['amount'] * 100) ?>
         <button
@@ -54,6 +44,16 @@
             </div>
         </button>
     <?php endforeach ?>
+    <button
+        class="active:scale-95 transition-transform hover:scale-[1.03]"
+        type="button"
+        onclick="showPanelGoal()">
+        <div class="flex items-center justify-center border-2 border-dashed hover:border-white border-gray-300 rounded-lg h-20 sm:h-24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="text-gray-300 w-10 h-10 sm:w-14 sm:h-14">
+                <path d="M4 12H20M12 4V20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </div>
+    </button>
 </main>
 <main id="goalPanel" class="hidden">
     <div id="goalOverlay" class="z-50 flex justify-center items-center fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50">
@@ -151,7 +151,7 @@
                     id="goalUpdateDescription"
                     name="goalUpdateDescription"
                     type="text"
-                    class="h-auto sm:h-11 text-left text-xl sm:text-4xl font-semibold textGray tlGreen focus:outline-none mr-8 sm:mr-10">
+                    class="h-auto sm:h-11 text-left text-2xl sm:text-4xl font-bold textGray tlGreen focus:outline-none mr-8 sm:mr-10">
                 <hr class="border-gray-300 mt-2 mb-5" />
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
                     <div class="flex items-center justify-center h-full">
@@ -207,6 +207,11 @@
                             max=""
                             step="0.01"
                             class="p-3 rounded-lg border border-gray-400 tlGreen focus:outline-none" />
+                        <div
+                            class="text-2xl sm:text-3xl font-semibold textGray"
+                            id="complete">
+                            Completed!!!
+                        </div>
                     </div>
                     <input
                         id="goalUpdateID"
@@ -225,18 +230,33 @@
                         type="hidden"
                         name="goalUpdateCategory">
                 </div>
-                <div class="mt-6 grid gap-x-8 sm:gap-x-16 grid-cols-2">
+                <div class="mt-6 grid grid-cols-1">
                     <button
-                        class=" py-1 text-base sm:text-xl font-semibold btGreen2 rounded-3xl"
-                        type="submit"
-                        name="goalDelete">
-                        Delete
-                    </button>
-                    <button
-                        class=" py-1 text-base sm:text-xl font-semibold btGreen2 rounded-3xl"
+                        class="px-10 py-1 text-lg sm:text-2xl font-semibold btGreen2 rounded-3xl justify-self-center"
                         type="submit"
                         name="goalUpdate">
-                        Save and Update
+                        Save
+                    </button>
+                    <button
+                        class="justify-self-end absolute active:scale-95 transition-transform hover:scale-105"
+                        type="submit"
+                        name="goalDelete">
+                        <svg
+                            class="h-8 w-8"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            version="1.1"
+                            id="_x32_"
+                            viewBox="0 0 512 512"
+                            xml:space="preserve">
+                            <g>
+                                <path fill="#d1d5db" class="st0" d="M88.594,464.731C90.958,491.486,113.368,512,140.234,512h231.523c26.858,0,49.276-20.514,51.641-47.269   l25.642-335.928H62.952L88.594,464.731z M420.847,154.93l-23.474,307.496c-1.182,13.37-12.195,23.448-25.616,23.448H140.234   c-13.42,0-24.434-10.078-25.591-23.132L91.145,154.93H420.847z" />
+                                <path fill="#d1d5db" class="st0" d="M182.954,435.339c5.877-0.349,10.35-5.4,9.992-11.269l-10.137-202.234c-0.358-5.876-5.401-10.349-11.278-9.992   c-5.877,0.357-10.35,5.409-9.993,11.277l10.137,202.234C172.033,431.231,177.085,435.696,182.954,435.339z" />
+                                <path fill="#d1d5db" class="st0" d="M256,435.364c5.885,0,10.656-4.763,10.656-10.648V222.474c0-5.885-4.771-10.648-10.656-10.648   c-5.885,0-10.657,4.763-10.657,10.648v202.242C245.344,430.601,250.115,435.364,256,435.364z" />
+                                <path fill="#d1d5db" class="st0" d="M329.046,435.339c5.878,0.357,10.921-4.108,11.278-9.984l10.129-202.234c0.348-5.868-4.116-10.92-9.993-11.277   c-5.877-0.357-10.92,4.116-11.277,9.992L319.054,424.07C318.697,429.938,323.17,434.99,329.046,435.339z" />
+                                <path fill="#d1d5db" class="st0" d="M439.115,64.517c0,0-34.078-5.664-43.34-8.479c-8.301-2.526-80.795-13.566-80.795-13.566l-2.722-19.297   C310.388,9.857,299.484,0,286.642,0h-30.651H225.34c-12.825,0-23.728,9.857-25.616,23.175l-2.721,19.297   c0,0-72.469,11.039-80.778,13.566c-9.261,2.815-43.357,8.479-43.357,8.479C62.544,67.365,55.332,77.172,55.332,88.38v21.926h200.66   h200.676V88.38C456.668,77.172,449.456,67.365,439.115,64.517z M276.318,38.824h-40.636c-3.606,0-6.532-2.925-6.532-6.532   s2.926-6.532,6.532-6.532h40.636c3.606,0,6.532,2.925,6.532,6.532S279.924,38.824,276.318,38.824z" />
+                            </g>
+                        </svg>
                     </button>
                 </div>
             </form>
@@ -298,6 +318,15 @@
         document.getElementById('goalUpdatePercent').textContent = `${percent}%`;
         document.getElementById('goalUpdatePreview').src = groupIcon;
         document.getElementById('goalUpdateIconPath').value = groupIcon;
+
+        if (percent == 100) {
+            document.getElementById('goalUpdatePaidAmountInput').classList.add('hidden');
+            document.getElementById('complete').classList.remove('hidden');
+        } else {
+            document.getElementById('goalUpdatePaidAmountInput').classList.remove('hidden');
+            document.getElementById('complete').classList.add('hidden');
+        }
+
         document.getElementById('goalUpdatePanel').classList.remove('hidden');
     }
 
