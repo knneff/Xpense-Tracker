24a872c869402d333a1d178842ce8baf65ee0904
knmnltzy <knmnltzy@gmail.com>
2024-11-30 16:46:32 +0800

    working notif system

--- controllers/noBars/notifications.php
+++ controllers/noBars/notifications.php
@@ -2,25 +2,34 @@
 
 $userID = $_SESSION['userid'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteNotif'])) {
-    $sql = "DELETE FROM notification WHERE userid = :userID";
-
-    $params = [
-        ':userID' => $userID,
-    ];
-
-    $db->query($sql, $params);
-
-    redirect($_SERVER['REQUEST_URI']);
-}
-
-$sql = "SELECT * FROM notification WHERE userID = :userID ORDER BY notifID DESC";
-
 $params = [
     ':userID' => $userID,
 ];
 
-$notifications = $db->query($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_SESSION['isViewed']) && $_SESSION['isViewed']) {
+    $sql = "UPDATE notification SET isViewed = 1 WHERE userID = :userID";
+
+    $db->query($sql, $params);
+
+    $_SESSION['isViewed'] = false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteNotif'])) {
+    $sql = "DELETE FROM notification WHERE userid = :userID";
+
+    $db->query($sql, $params);
+    redirect($_SERVER['REQUEST_URI']);
+}
+
+$sqlNotif = "SELECT * FROM notification WHERE userID = :userID ORDER BY notifID DESC";
+$sqlCount = "SELECT COUNT(*) AS unviewed FROM notification WHERE isViewed = 0 AND userID =:userID";
+
+$notifications = $db->query($sqlNotif, $params)->fetchAll(PDO::FETCH_ASSOC);
+$count = $db->query($sqlCount, $params)->fetch(PDO::FETCH_ASSOC);
+
+
+
+
 ?>
 
 
--- views/partials/notifications.view.php
+++ views/partials/notifications.view.php
@@ -4,8 +4,9 @@
     <div
         class="grid items-center textGray h-5/6 w-9 sm:w-10">
         <div
-            class="text-xs scale-50 sm:scale-75 w-7 py-1 absolute justify-self-end self-start bg-gray-300 text-gray-700 font-semibold rounded-full">
-            1
+            id="notifCountDisplay"
+            class="<?= $count['unviewed'] ? 'bg-gray-300' : 'hidden'; ?> text-xs scale-50 sm:scale-75 w-7 py-1 absolute justify-self-end self-start  text-gray-700 font-semibold rounded-full">
+            <?= $count['unviewed'] ?>
         </div>
         <svg class="size-7 sm:size-8 text-gray" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
             <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
@@ -19,7 +20,7 @@
         </div>
         <div class="text-sm sm:text-base self-end hover:text-white">
             <form method="POST">
-                <button 
+                <button
                     name="deleteNotif"
                     type="submit"
                     class="hover:underline">
@@ -30,7 +31,7 @@
     </div>
     <div class="scrollbar-custom max-h-96 overflow-auto gap-2 grid">
         <?php foreach ($notifications as $notification) : ?>
-            <div class="<?= $notification['isViewed'] ? 'bgGreen' : 'bg-emerald-900'; ?> mr-2 sm:mr-4 p-2 rounded-md">
+            <div class="<?= $notification['isViewed'] ? 'bgGreen' : 'bg-emerald-900'; ?> notifPanel mr-2 sm:mr-4 p-2 rounded-md">
                 <div class="font-semibold text-base sm:text-lg">
                     <?= $notification['title'] ?>
                 </div>
@@ -42,17 +43,27 @@
     </div>
 </div>
 <script>
+    let isNotifDisplayed = false;
     const buttonNotif = document.querySelector("#buttonNotif");
     const panelNotif = document.querySelector("#panelNotif");
 
     buttonNotif.addEventListener("click", function(event) {
         event.stopPropagation();
-        panelNotif.classList.toggle("hidden");
+        panelNotif.classList.remove("hidden");
+        isNotifDisplayed = true;
     });
 
     document.addEventListener("click", function(event) {
-        if (!panelNotif.contains(event.target) && event.target !== buttonNotif) {
+        if (!panelNotif.contains(event.target) && event.target !== buttonNotif && isNotifDisplayed) {
             panelNotif.classList.add("hidden");
+            document.getElementById("notifCountDisplay").classList.add("hidden");
+
+            document.querySelectorAll('.notifPanel').forEach(panel => {
+                panel.classList.add('bgGreen');
+                panel.classList.remove('bg-emerald-900');
+            });
+            <?php $_SESSION['isViewed'] = true; ?>
+            isNotifDisplayed = false;
         }
     });
 </script>
