870486ddda0bd6bae69d4b13a14b170d0521ffcf
Add852 <xadd852x@gmail.com>
2024-11-30 12:20:14 +0800

    show invite link expiry time

--- views/sharedExpense.view.php
+++ views/sharedExpense.view.php
@@ -269,25 +269,30 @@
                 </button>
               </form>
             ";
-          } else {
-            echo "<button onclick='showInvite()' class='flex flex-row justify-center w-full py-2 px-2 items-center gap-1 btGreen rounded-lg'>
-                <svg class='size-5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
-                  <path stroke-linecap='round' stroke-linejoin='round' d='M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244' />
-                </svg>
-                <p class='pb-1'>Show Invite Link</p>
-              </button>
+          } else { //there's an ongoing invite link and not expired
+            $interval = $currentDateTime->diff($tokenExpiry);
+            $minutes = $interval->i;
+            echo "<div class='flex flex-col justify-center items-center'>
+                <button onclick='showInvite()' class='flex flex-row justify-center w-full py-2 px-2 items-center gap-1 btGreen rounded-lg'>
+                  <svg class='size-5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                    <path stroke-linecap='round' stroke-linejoin='round' d='M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244' />
+                  </svg>
+                  <p class='pb-1'>Show Invite Link</p>
+                </button>
+                <p class='text-sm textRed'>Expires in $minutes minutes</p>
+              </div>
             ";
             require('views/partials/shared/invite.view.php');
           }
         } else { // when there's no onoing invite link
           echo "<form method='POST' action='/gen_invite'>
               <input name='groupID' value='$groupID' hidden>
-              <button type='submit' class='flex flex-row justify-center w-full py-2 px-2 items-center gap-1 btGreen rounded-lg'>
-                <svg class='size-5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
-                  <path stroke-linecap='round' stroke-linejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-                </svg>
-                <p class='pb-1'>Generate Invite Link</p>
-              </button>
+                <button type='submit' class='flex flex-row justify-center w-full py-2 px-2 items-center gap-1 btGreen rounded-lg'>
+                  <svg class='size-5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                    <path stroke-linecap='round' stroke-linejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+                  </svg>
+                  <p class='pb-1'>Generate Invite Link</p>
+                </button>
             </form>
           ";
         }
