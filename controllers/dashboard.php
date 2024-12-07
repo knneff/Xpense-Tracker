be2316b8ac219ed60bd930ad1a2f507ce3afec16
knmnltzy <knmnltzy@gmail.com>
2024-12-07 17:06:55 +0800

    groupicon na goal

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -20,7 +20,7 @@ $subscriptions = $db->query('select amount, description, period, category from s
 $pendingSubCount = $db->query("SELECT COUNT(expensesHistory.expenseID) from expensesHistory JOIN users ON expensesHistory.userID = users.userid WHERE expensesHistory.expenseType=? AND users.userid=? AND expensesHistory.expenseState=? ORDER BY expenseTime ASC;", ['subscription', $userID, 'pending'])->fetchColumn();
 
 // for goals tile
-$goals = $db->query('select amount, description, paidAmount from goals WHERE userid=? ORDER BY goalID DESC LIMIT 6;', [$userID])->fetchAll(PDO::FETCH_ASSOC);
+$goals = $db->query('select amount, description, paidAmount, groupIcon from goals WHERE userid=? ORDER BY goalID DESC LIMIT 6;', [$userID])->fetchAll(PDO::FETCH_ASSOC);
 
 // for groups tile
 $groups = $db->query("SELECT clan.groupName, clan.groupIcon, clan.groupID FROM clan join clanMembers ON clan.groupID=clanMembers.groupID WHERE clanMembers.userid = ? ORDER BY clan.groupID DESC LIMIT 3", [$userID])->fetchAll(PDO::FETCH_ASSOC);
