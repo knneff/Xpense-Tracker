4e1a9dd29bf967c969c8f2c2f5a2fa8508796c0a
Add852 <xadd852x@gmail.com>
2024-12-11 15:13:09 +0800

    sidebar and aboutus konte

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -96,7 +96,7 @@
     </a>
 
     <!-- AUDIT LOG -->
-    <a href="/audit" title="Audit Log" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/audit") ? 'bg-emerald-900' : '' ?>">
+    <a href="/audit" title="Audit Log" class="<?= ($_SESSION['userid'] == 4 || $_SESSION['userid'] == 25) ? 'flex' : 'hidden' ?> hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/audit") ? 'bg-emerald-900' : '' ?>">
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="textGray w-12 p-3">
             <path fill-rule="evenodd" d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 18.375V5.625ZM21 9.375A.375.375 0 0 0 20.625 9h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5ZM10.875 18.75a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5ZM3.375 15h7.5a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375Zm0-3.75h7.5a.375.375 0 0 0 .375-.375v-1.5A.375.375 0 0 0 10.875 9h-7.5A.375.375 0 0 0 3 9.375v1.5c0 .207.168.375.375.375Z" clip-rule="evenodd" />
@@ -107,7 +107,7 @@
     </a>
 
     <!-- Tickets -->
-    <a href="/tickets" title="Tickets" class="hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/tickets") ? 'bg-emerald-900' : '' ?>">
+    <a href="/tickets" title="Tickets" class="<?= ($_SESSION['userid'] == 4 || $_SESSION['userid'] == 25) ? 'flex' : 'hidden' ?> hover:bg-emerald-900 transition flex flex-row items-center <?= isUri("/tickets") ? 'bg-emerald-900' : '' ?>">
 
         <svg class="size-12 p-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 
