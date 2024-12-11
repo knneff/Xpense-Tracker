4b15c64208501a3e144d17d31807be25d4a599f6
kit <keithrussel632@gmail.com>
2024-12-11 15:15:36 +0800

    anthony about us

--- views/aboutUs.view.php
+++ views/aboutUs.view.php
@@ -63,15 +63,15 @@
                 <!-- Manager 1 -->
                 <div class="text-center">
                     <img class="w-40 h-40 mx-auto rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="assets\1x1\anthony.png" alt="Anthony Dayrit">
-                    <h3 class="mt-4 text-xl font-bold text-green-400">Anthony</h3>
-                    <p class="mt-2 text-gray-400 text-lg">UI</p>
+                    <h3 class="mt-4 text-xl font-bold text-green-400">Anthony Dayrit</h3>
+                    <p class="mt-2 text-gray-400 text-lg">Full Stack Developer</p>
                     <div class="mt-4 flex justify-center flex-col space-y-2">
                         <!-- Email -->
                         <div class="flex items-center justify-center space-x-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                            <a href="mailto:skibidi@company.com" class="text-gray-400 hover:text-green-400">skibidi@company.com</a>
+                            <a href="mailto:xadd852x@gmail.com" class="text-gray-400 hover:text-green-400">xadd852x@gmail.com</a>
                         </div>
 
                         <!-- GitHub -->
@@ -81,7 +81,7 @@
                                 <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
                                 </svg>
-                                <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">skbidi.com</a>
+                                <a href="https://github.com/Add852" class="text-gray-400 hover:text-green-400">Add852</a>
                             </a>
                         </div>
                     </div>
@@ -122,7 +122,7 @@
                 <!-- Manager 2 -->
                 <div class="text-center m-0 transform transition-transform duration-300 hover:scale-105">
                     <img class="w-40 h-40 mx-auto rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="assets\1x1\eicee.png" alt="Eicee Lance Bautsista">
-                    <h3 class="mt-4 text-xl font-bold text-green-400">Eicee Lance Austin S. Baustista</h3>
+                    <h3 class="mt-4 text-xl font-bold text-green-400">Eicee Baustista</h3>
                     <p class="mt-2 text-gray-400 text-lg">UI/Frontend Leader</p>
                     <div class="mt-4 flex justify-center flex-col space-y-2">
                         <!-- Email -->
@@ -148,7 +148,7 @@
                 <!-- Manager 3 -->
                 <div class="text-center m-0 transform transition-transform duration-300 hover:scale-105">
                     <img class="w-40 h-40 mx-auto rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="assets\1x1\matt.png" alt="Matt Angel Neil Ricohermoso">
-                    <h3 class="mt-4 text-xl font-bold text-green-400">Matt Angel Neil P. Ricohermoso</h3>
+                    <h3 class="mt-4 text-xl font-bold text-green-400">Matt Ricohermoso</h3>
                     <p class="mt-2 text-gray-400 text-lg">Front-End Developer and Project Manager</p>
                     <div class="mt-4 flex justify-center flex-col space-y-2">
                         <!-- Email -->
