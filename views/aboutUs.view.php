180fc3c0b96ca49588612066c08b17e0641660ea
Add852 <xadd852x@gmail.com>
2024-12-11 14:37:31 +0800

    some abt us

--- views/aboutUs.view.php
+++ views/aboutUs.view.php
@@ -1,7 +1,7 @@
 <?php require('partials/bodyNoBars.php') ?>
 
 <!-- ABOUT US -->
-<section class='h-full flex flex-col justify-center gap-2 sm:gap-6 px-4 sm:px-24'>
+<section class='py-8 sm:py-0 sm:h-full flex flex-col justify-center gap-2 sm:gap-6 px-4 sm:px-24'>
 
     <!-- About Us -->
     <h1 class="text-white text-center text-3xl sm:text-4xl md:text-5xl font-extrabold text-shadow-lg leading-tight">
@@ -32,6 +32,7 @@
         </div>
     </section>
 
+    <!-- ARROW -->
     <div class="flex justify-center w-full bottom-0 items-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 animate-ping" viewBox="0 0 1024 1024" version="1.1" fill="#ffffff">
             <g id="SVGRepo_bgCarrier" stroke-width="0" />
@@ -281,6 +282,114 @@
             </div>
         </div>
         <!-- Developer 3 -->
+        <div class="bgGreen gap-2 p-2 rounded-xl flex flex-row transform transition-transform duration-300 hover:scale-105">
+            <!-- IMAGE -->
+            <img class="w-40 h-40 rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="team-member8.jpg" alt="Max">
+            <!-- INFO -->
+            <div class='flex flex-col justify-start'>
+                <!-- Name -->
+                <h3 class="mt-2 text-xl font-bold text-green-400">Max</h3>
+                <!-- Role -->
+                <p class="text-gray-400 text-lg">UI</p>
+                <!-- Email -->
+                <div class="mt-2 flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <a href="mailto:skibidi@company.com" class="text-gray-400 hover:text-green-400">skibidi@company.com</a>
+                </div>
+                <!-- Phone -->
+                <div class="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                    </svg>
+                    <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">+1 (234) 567-890</a>
+                </div>
+                <!-- GitHub -->
+                <div class="flex gap-2">
+                    <a href="https://github.com/skibidi" rel="noreferrer" target="_blank" class="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                        <span class="sr-only">GitHub</span>
+                        <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                        </svg>
+                        <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">skbidi.com</a>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Developer 4 -->
+        <div class="bgGreen gap-2 p-2 rounded-xl flex flex-row transform transition-transform duration-300 hover:scale-105">
+            <!-- IMAGE -->
+            <img class="w-40 h-40 rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="team-member8.jpg" alt="Max">
+            <!-- INFO -->
+            <div class='flex flex-col justify-start'>
+                <!-- Name -->
+                <h3 class="mt-2 text-xl font-bold text-green-400">Max</h3>
+                <!-- Role -->
+                <p class="text-gray-400 text-lg">UI</p>
+                <!-- Email -->
+                <div class="mt-2 flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <a href="mailto:skibidi@company.com" class="text-gray-400 hover:text-green-400">skibidi@company.com</a>
+                </div>
+                <!-- Phone -->
+                <div class="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                    </svg>
+                    <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">+1 (234) 567-890</a>
+                </div>
+                <!-- GitHub -->
+                <div class="flex gap-2">
+                    <a href="https://github.com/skibidi" rel="noreferrer" target="_blank" class="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                        <span class="sr-only">GitHub</span>
+                        <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                        </svg>
+                        <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">skbidi.com</a>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Developer 5 -->
+        <div class="bgGreen gap-2 p-2 rounded-xl flex flex-row transform transition-transform duration-300 hover:scale-105">
+            <!-- IMAGE -->
+            <img class="w-40 h-40 rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="team-member8.jpg" alt="Max">
+            <!-- INFO -->
+            <div class='flex flex-col justify-start'>
+                <!-- Name -->
+                <h3 class="mt-2 text-xl font-bold text-green-400">Max</h3>
+                <!-- Role -->
+                <p class="text-gray-400 text-lg">UI</p>
+                <!-- Email -->
+                <div class="mt-2 flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <a href="mailto:skibidi@company.com" class="text-gray-400 hover:text-green-400">skibidi@company.com</a>
+                </div>
+                <!-- Phone -->
+                <div class="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                    </svg>
+                    <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">+1 (234) 567-890</a>
+                </div>
+                <!-- GitHub -->
+                <div class="flex gap-2">
+                    <a href="https://github.com/skibidi" rel="noreferrer" target="_blank" class="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                        <span class="sr-only">GitHub</span>
+                        <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                        </svg>
+                        <a href="tel:+1234567890" class="text-gray-400 hover:text-green-400">skbidi.com</a>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Developer 6 -->
         <div class="bgGreen gap-2 p-2 rounded-xl flex flex-row transform transition-transform duration-300 hover:scale-105">
             <!-- IMAGE -->
             <img class="w-40 h-40 rounded-full border-4 border-green-800 object-cover shadow-lg hover:shadow-xl transition-shadow duration-300" src="team-member8.jpg" alt="Max">
